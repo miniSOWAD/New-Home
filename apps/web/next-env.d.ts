b1d4caa41e6a3cb9af8c0ae88dc4ceba74e0c9c2
Md Mahruf Alam <baisakh2015@gmail.com>
2026-05-30 01:52:46 +0600

    web preferences

---
+++ apps/web/next-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited manually.
+// It is required for Next.js TypeScript support.
