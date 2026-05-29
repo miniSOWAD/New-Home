69d7ea6001d54de14ecc234e8da55a533245544c
Md Mahruf Alam <baisakh2015@gmail.com>
2026-05-30 02:24:30 +0600

    web schema

---
+++ apps/web/src/schemas/index.ts
@@ -0,0 +1,7 @@
+export * from "./auth.schema";
+export * from "./user.schema";
+export * from "./tolet.schema";
+export * from "./service.schema";
+export * from "./review.schema";
+export * from "./report.schema";
+export * from "./contact.schema";
