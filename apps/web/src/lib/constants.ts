9e0b99cec7b8d9c8ba21b7787f1c01baaff5b308
Md Mahruf Alam <baisakh2015@gmail.com>
2026-05-30 02:20:53 +0600

    web lib

---
+++ apps/web/src/lib/constants.ts
@@ -0,0 +1,13 @@
+export const APP_NAME = "New Home";
+
+export const ACCESS_TOKEN_COOKIE = "new_home_access_token";
+export const REFRESH_TOKEN_COOKIE = "new_home_refresh_token";
+export const USER_ROLE_COOKIE = "new_home_user_role";
+export const APPROVAL_STATUS_COOKIE = "new_home_approval_status";
+
+export const DEFAULT_PAGE = 1;
+export const DEFAULT_LIMIT = 12;
+
+export const WAITING_APPROVAL_MESSAGE = "Your account is waiting for approval.";
+export const REJECTED_ACCOUNT_MESSAGE = "Your account request was rejected.";
+export const SUSPENDED_ACCOUNT_MESSAGE = "Your account has been suspended.";
